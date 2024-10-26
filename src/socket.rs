f1fc92026d62f5b087aaa16b074f681875ef7846
Callum Mackenzie <alexxander1611@gmail.com>
2024-10-26 00:08:12 -0700

    Add comments to socket.rs

--- src/socket.rs
+++ src/socket.rs
@@ -3,17 +3,27 @@ use std::net::{SocketAddr, UdpSocket};
 
 const DYNAMIC_SOCKET_BUFFER_BYTES: usize = 32 * 32;
 
+/// A trait representing a game socket which data is sent/recieved from.
+/// Trait created to allow for dependency injection during testing.
+pub trait GameSocket {
+	/// Creates a new GameSocket from the given UdpSocket
+	fn new(socket: UdpSocket) -> Self;
+	/// Recieves bytes and returns their size and the address they originated from.
+	fn recv_from(&mut self) -> std::io::Result<(usize, SocketAddr)>;
+}
+
+/// Dynamic UDP socket using a UdpSocket recieving packets from a client.
 pub struct DynamicUDPSocket {
     buffer: [u8; DYNAMIC_SOCKET_BUFFER_BYTES],
     socket: UdpSocket,
 }
 
-impl DynamicUDPSocket {
-    pub fn new(socket: UdpSocket) -> Self {
+impl GameSocket for DynamicUDPSocket {
+    fn new(socket: UdpSocket) -> Self {
 		Self { buffer: [0; DYNAMIC_SOCKET_BUFFER_BYTES], socket }
 	}
 	
-	pub fn recv_from(&mut self) -> std::io::Result<(usize, SocketAddr)> {
+	fn recv_from(&mut self) -> std::io::Result<(usize, SocketAddr)> {
         let (amt, src) = self.socket.recv_from(&mut self.buffer)?;
         Ok((amt, src))
     }
