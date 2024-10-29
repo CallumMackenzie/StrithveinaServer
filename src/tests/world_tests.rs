e504a05617fec6f8d375f4d7bbcc856335e28939
Eric George <erickurian@gmail.com>
2024-10-29 11:44:23 -0600

    fixed pull request changesgit status!

--- src/tests/world_tests.rs
+++ src/tests/world_tests.rs
@@ -1,49 +1,4 @@
-use crate::world::get_data_from_image_cell;
-
 #[test]
 fn a_world_test() {
     assert_eq!(1, 1);
-}
-
-#[test]
-//contains optional println!'s for debugging
-fn ensure_proper_bit_collection_from_image_cell() {
-    let mut num: u32 = 0b10000011000000000000000010111111;
-    let mut real_parta: u32 = 0b1000001;
-    let mut real_partb: u32 = 0b100000000000000001;
-    let mut real_partc: u32 = 0b111111;
-    let (parta, partb, partc) = get_data_from_image_cell(num);
-
-    // println!("Original number: {:32b}", num);
-    // println!("Extracted 7-bit part: {:7b}", parta); // Print in binary
-    // println!("Extracted 18-bit part: {:18b}", partb); // Print in binary
-    // println!("Extracted 6-bit part: {:6b}", partc); // Print in binary
-
-    assert_eq!(parta, real_parta);
-    assert_eq!(partb, real_partb);
-    assert_eq!(partc, real_partc);
-
-    num = !num; // 0b01111100111111111111111101000000
-    real_parta = 0b0111110;
-    real_partb = 0b011111111111111110;
-    real_partc = 0b000000;
-    let (parta, partb, partc) = get_data_from_image_cell(num);
-
-    println!("Original number: {:32b}", num);
-    println!(
-        "Extracted 7-bit part: {:7b}, compared to: {:7b}",
-        parta, real_parta
-    ); // Print in binary
-    println!(
-        "Extracted 18-bit part: {:18b}, compared to: {:18b}",
-        partb, real_partb
-    ); // Print in binary
-    println!(
-        "Extracted 6-bit part: {:6b}, compared to: {:6b}",
-        partc, real_partc
-    ); // Print in binary
-
-    assert_eq!(parta, real_parta);
-    assert_eq!(partb, real_partb as u32);
-    assert_eq!(partc, real_partc as u32);
 }
--- src/world.rs
+++ src/world.rs
@@ -1,5 +1,3 @@
-const WORLD_WIDTH: u32 = 2000;
-const WORLD_LENGTH: u32 = 2000;
 //base world size dimensions -> is necessary to determine image[] length
 
 const TILE_TYPE_BITS: u32 = 7;
@@ -29,8 +27,24 @@ trait WorldGenerator {
 fn create_mask(bit_amt: u32) -> u32 {
     (1 << bit_amt) - 1
 }
+
+/**
+ * Tile type used on const enum of all the tile types implemented
+ * Tile instance goes to Tile[] for the specific created instances
+ * EntitySpawnInfo is a collection of flags
+ */
+
+type TileTypeIndex = u32;
+type TileInstanceIndex = u32;
+type EntitySpawnInfoIndex = u32;
+struct ImageCellData {
+    tile_type_index: TileTypeIndex,
+    tile_instance_index: TileInstanceIndex,
+    entity_spawn_info_index: EntitySpawnInfoIndex,
+}
+
 //do functions need to be pub to be tested?
-pub fn get_data_from_image_cell(bits: u32) -> (u32, u32, u32) {
+fn get_data_from_image_cell(bits: u32) -> ImageCellData {
     let entity_spawn_info_mask: u32 = create_mask(ENTITY_SPAWN_INFO_BITS);
     let entity_spawn_info = bits & entity_spawn_info_mask;
 
@@ -41,5 +55,60 @@ pub fn get_data_from_image_cell(bits: u32) -> (u32, u32, u32) {
     let tile_type =
         (bits >> (ENTITY_SPAWN_INFO_BITS + UNUSED_BITS + TILE_INSTANCE_BITS)) & tile_type_mask;
 
-    (tile_type, tile_instance, entity_spawn_info)
+    ImageCellData {
+        tile_type_index: (tile_type),
+        tile_instance_index: (tile_instance),
+        entity_spawn_info_index: (entity_spawn_info),
+    }
+}
+
+#[test]
+//contains optional println!'s for debugging
+fn ensure_proper_bit_collection_from_image_cell() {
+    let mut num: u32 = 0b10000011000000000000000010111111;
+    let mut real_parta: u32 = 0b1000001;
+    let mut real_partb: u32 = 0b100000000000000001;
+    let mut real_partc: u32 = 0b111111;
+    let ImageCellData {
+        tile_type_index,
+        tile_instance_index,
+        entity_spawn_info_index,
+    } = get_data_from_image_cell(num);
+
+    // println!("Original number: {:32b}", num);
+    // println!("Extracted 7-bit part: {:7b}", parta); // Print in binary
+    // println!("Extracted 18-bit part: {:18b}", partb); // Print in binary
+    // println!("Extracted 6-bit part: {:6b}", partc); // Print in binary
+
+    assert_eq!(tile_type_index, real_parta);
+    assert_eq!(tile_instance_index, real_partb);
+    assert_eq!(entity_spawn_info_index, real_partc);
+
+    num = !num; // 0b01111100111111111111111101000000
+    real_parta = 0b0111110;
+    real_partb = 0b011111111111111110;
+    real_partc = 0b000000;
+    let ImageCellData {
+        tile_type_index,
+        tile_instance_index,
+        entity_spawn_info_index,
+    } = get_data_from_image_cell(num);
+
+    println!("Original number: {:32b}", num);
+    println!(
+        "Extracted 7-bit part: {:7b}, compared to: {:7b}",
+        tile_type_index, real_parta
+    ); // Print in binary
+    println!(
+        "Extracted 18-bit part: {:18b}, compared to: {:18b}",
+        tile_instance_index, real_partb
+    ); // Print in binary
+    println!(
+        "Extracted 6-bit part: {:6b}, compared to: {:6b}",
+        entity_spawn_info_index, real_partc
+    ); // Print in binary
+
+    assert_eq!(tile_type_index, real_parta);
+    assert_eq!(tile_instance_index, real_partb as u32);
+    assert_eq!(entity_spawn_info_index, real_partc as u32);
 }
